7e8498e751bc37e386ee27ff989714cf8693ad83
codesoap <codesoap@mailbox.org>
2020-09-07 22:53:02 +0200

    Apply gofmt

--- util/builtins.go
+++ util/builtins.go
@@ -1,29 +1,32 @@
 package util
 
 import (
-	"time"
 	"fmt"
-    "github.com/shirou/gopsutil/mem"
 	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
+	"time"
 )
+
 //blockId is automatically allocated
 //send channel is used to update blocks data
 //Gothreads are waked up by messages on rec channels
 //action is a map of whatever was in action json object for corressponding action
-var FunctionMap = map[string]func(blockId int, send chan Change, rec chan bool, action map[string]interface{}) {
-	"#Date":Date,
-	"#Memory":Memory,
-	"#Cpu":Cpu,
+var FunctionMap = map[string]func(blockId int, send chan Change, rec chan bool, action map[string]interface{}){
+	"#Date":   Date,
+	"#Memory": Memory,
+	"#Cpu":    Cpu,
 }
+
 //Update time according to "format" property
 func Date(blockId int, send chan Change, rec chan bool, action map[string]interface{}) {
 	run := true
 	for run {
 		send <- Change{blockId, time.Now().Format(action["format"].(string)), true}
 		//Block untill other thread will ping you
-		run = <- rec
+		run = <-rec
 	}
 }
+
 //Get current % of used memory
 func Memory(blockId int, send chan Change, rec chan bool, action map[string]interface{}) {
 	run := true
@@ -31,9 +34,10 @@ func Memory(blockId int, send chan Change, rec chan bool, action map[string]inte
 		v, _ := mem.VirtualMemory()
 		send <- Change{blockId, fmt.Sprintf(action["format"].(string), v.UsedPercent), true}
 		//Block untill other thread will ping you
-		run = <- rec
+		run = <-rec
 	}
 }
+
 //Get current % of used CPU
 func Cpu(blockId int, send chan Change, rec chan bool, action map[string]interface{}) {
 	run := true
@@ -41,6 +45,6 @@ func Cpu(blockId int, send chan Change, rec chan bool, action map[string]interfa
 		val, _ := cpu.Percent(time.Second, false)
 		send <- Change{blockId, fmt.Sprintf(action["format"].(string), val[0]), true}
 		//Block untill other thread will ping you
-		run = <- rec
+		run = <-rec
 	}
 }
--- util/utils.go
+++ util/utils.go
@@ -1,23 +1,24 @@
 package util
 
 import (
-	"os/exec"
-	"time"
-	"strings"
 	"fmt"
-	"os/signal"
 	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
 	"syscall"
+	"time"
 )
 
 type Change struct {
 	BlockId int
-	Data string
+	Data    string
 	Success bool
 }
+
 //Based on "timer" prorty from config file
 //Schedule gothread that will ping other gothreads via send channel
-func Schedule(send chan bool, duration string){
+func Schedule(send chan bool, duration string) {
 	u, err := time.ParseDuration(duration)
 	if err == nil {
 		for {
@@ -25,25 +26,27 @@ func Schedule(send chan bool, duration string){
 			time.Sleep(u)
 		}
 	} else {
-		fmt.Println("Couldn't set a scheduler due to improper time format: "+ duration)
+		fmt.Println("Couldn't set a scheduler due to improper time format: " + duration)
 	}
 }
+
 //Run any arbitrary POSIX shell command
-func RunCmd(blockId int, send chan Change, rec chan bool, action map[string]interface{} ) {
+func RunCmd(blockId int, send chan Change, rec chan bool, action map[string]interface{}) {
 	cmdStr := action["command"].(string)
 	run := true
 
 	for run {
-		out, err := exec.Command("sh","-c",cmdStr).Output()
+		out, err := exec.Command("sh", "-c", cmdStr).Output()
 		if err == nil {
 			send <- Change{blockId, strings.TrimSuffix(string(out), "\n"), true}
 		} else {
 			send <- Change{blockId, err.Error(), false}
 		}
 		//Block untill other thread will ping you
-		run = <- rec
+		run = <-rec
 	}
 }
+
 //Create a channel that captures all 34-64 signals
 func GetSIGRTchannel() chan os.Signal {
 	sigChan := make(chan os.Signal, 1)
